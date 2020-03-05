c4bfe54dc5c99c52ef9b0438e3fbe817cf8b8c6a
Bogdan Pechenyk <bogdanpechenyk@gmail.com>
2020-03-05 22:28:55 +0200

    Updates

---
+++ precache-manifest.91989f7ac44603687c53cc3387a0de63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23569d226d0f74ea98c9b3bb4af4aff3",
+    "url": "/goit-react-hw-01-components-historyTransaction/index.html"
+  },
+  {
+    "revision": "346671f60b4995494368",
+    "url": "/goit-react-hw-01-components-historyTransaction/static/css/main.ce6b722c.chunk.css"
+  },
+  {
+    "revision": "093a04894e3e2d800561",
+    "url": "/goit-react-hw-01-components-historyTransaction/static/js/2.2e74d4eb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-01-components-historyTransaction/static/js/2.2e74d4eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "346671f60b4995494368",
+    "url": "/goit-react-hw-01-components-historyTransaction/static/js/main.76b90baf.chunk.js"
+  },
+  {
+    "revision": "8f91d25113e614b99252",
+    "url": "/goit-react-hw-01-components-historyTransaction/static/js/runtime-main.246a4e1f.js"
+  }
+]);
